9c5c897182da56bc8cd71a19d896b08069660f4e
JoancitoGit <joan.garced@gmail.com>
2025-04-24 13:27:17 -0700

    To practice at home

---
+++ 01-Fundamentals-Part-1/starter/assigments.js
@@ -0,0 +1,32 @@
+//ASSIGMENTS
+const country = "México";
+const continent = "América";
+let population = 18000000;
+
+const isIsland = true;
+let lenguage = "Español";
+lenguage = "English";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(lenguage);
+
+population /= 2;
+console.log(population);
+population += 1;
+console.log(population);
+let finlandPopulation = 6000000;
+console.log(population > finlandPopulation);
+const averagePopulation = 33000000;
+console.log(population < averagePopulation);
+const description =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " million people speak " +
+  lenguage;
+console.log(description);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,128 @@
+//TAKING DECISIONS IF / ELSE STATEMENTS
+const age = 15;
+
+if (age >= 18) {
+  console.log("Sarah can start driving license");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+// //Strings and Template Literals
+// const firstName = "Jonas";
+// const job = "Teacher";
+// const birthYear = 1991;
+// const year = 2037;
+
+// const jonas =
+//   "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+// console.log(jonas);
+
+// const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+// console.log(jonasNew);
+
+// console.log(`Just a regular string...`);
+
+// console.log(
+//   "String with \n\
+//   multiple \n\
+//   lines"
+// );
+
+// console.log(`---------
+// Multiple
+// Lines
+// String
+// ---------`);
+
+//OPERATORS PRECEDENCE
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+// console.log(25 - 10 - 5);
+
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10
+// console.log(x, y);
+
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah, averageAge);
+//OPERATORS
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+// //Math operators
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+// console.log(ageJonas * 2, ageJonas / 2, 2 ** 3);
+
+// const firstName = "Jonas";
+// const lastName = "Schmedtmann";
+// console.log(firstName + " " + lastName);
+
+// // Assigment operators
+// let x = 10 + 5;
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1;
+// x--; // x = x - 1;
+// x--;
+// console.log(x);
+
+// // Comparison operators produce a Boolean
+// console.log(ageJonas > ageJonas); // > , <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+// console.log(now - 1991 > now - 2019);
+
+//LET, CONST AND VAR
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+
+// // const job;
+
+// var job = "Programmer";
+// job = "Teacher";
+
+// lastName = "Schmedtmann";
+// console.log(lastName);
+//DATA TYPES
+// let javascriptIsFun = true;
+// // console.log(javascriptIsFun);
+// // console.log(typeof true);
+// // console.log(typeof javascriptIsFun);
+// // // console.log(typeof 23);
+// // // console.log(typeof "Jonas");
+
+// // javascriptIsFun = "YES";
+// // console.log(typeof javascriptIsFun);
+// let year;
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+//VARIABLES AND CONSOLE.LOG() FUNCTION
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Matilda";
+// console.log(firstName);
+
+// let PI = 3.1416;
+
+// let myFirstJob = "Front End Developer";
+// let myCurrentJob = "Full Stack Developer";
+
+// console.log(myCurrentJob);
