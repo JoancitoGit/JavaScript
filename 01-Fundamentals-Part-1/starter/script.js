634269a44b1a982d674b81e0651e65f0d47f03d3
JoancitoGit <joan.garced@gmail.com>
2025-04-28 10:00:27 -0700

    If ELSE

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -7,6 +7,17 @@ if (age >= 18) {
   const yearsLeft = 18 - age;
   console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
 }
+
+const birthYear = 1991;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
+
 // //Strings and Template Literals
 // const firstName = "Jonas";
 // const job = "Teacher";
