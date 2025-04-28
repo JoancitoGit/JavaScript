f307b233fef8560c345daefca6c96d68f0a4a5fb
JoancitoGit <joan.garced@gmail.com>
2025-04-28 10:28:11 -0700

    Truthy and Falsy Values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,21 +1,43 @@
+//TRUTHY AND FALSY VALUES
+//5 falsy values 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+  console.log("don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height;
+if (height) {
+  console.log("Yay! Height is defined!");
+} else {
+  console.log("Height is UNDEFINED");
+}
+
 //TYPE CONVERSION
-const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
 
-console.log(Number("Jonas"));
-console.log(typeof NaN);
+// console.log(Number("Jonas"));
+// console.log(typeof NaN);
 
-console.log(String(23), 23);
-//TYPE COERCION
-console.log("I am " + 23 + " years old");
-console.log("I am " + String(23) + " years old");
-console.log("23" - "10" - 3);
-console.log("23" / "2");
+// console.log(String(23), 23);
+// //TYPE COERCION
+// console.log("I am " + 23 + " years old");
+// console.log("I am " + String(23) + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" / "2");
 
-let n = "1" + 1;
-n = n - 1;
-console.log(n);
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
 // //TAKING DECISIONS IF / ELSE STATEMENTS
 // const age = 15;
 
