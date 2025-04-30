02544e25fa6fe5ab54e8c4ee3722b5e5ffd3d0b1
JoancitoGit <joan.garced@gmail.com>
2025-04-30 16:52:39 -0700

    Arrow Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,19 @@
+//CALLING FUNCTIONS INSIDE OTHER FUNCTIONS
+
+// ARROW FUNCTION
+// const calcAge3 = (birthYear) => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   // return retirement;
+//   return `${firstName} retires in ${retirement} years`;
+// };
+
+// console.log(yearsUntilRetirement(1991, "Joan"));
+
 // // FUNCTION EXPRESSIONS
 
 // function calcAge1(birthYear) {
