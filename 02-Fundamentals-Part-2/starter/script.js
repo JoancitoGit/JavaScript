a4e04123086326fc0acfe7c009a6b94f497d66d0
JoancitoGit <joan.garced@gmail.com>
2025-04-30 11:13:35 -0700

    Strict mode and Functions

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,22 @@
+//FUNCTIONS
+"use strict";
+
+function logger() {
+  console.log("My name is Joan");
+}
+// Calling / Running / Invoking function
+logger();
+logger();
+
+// // Strict Mode
+// "use strict";
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive :D");
+
+// // const interface = "Audio";
+// // const private = 2452;
+// // const if = 23;
