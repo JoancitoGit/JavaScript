a19d1935955ece171d3daa64abe304520af05d84
JoancitoGit <joan.garced@gmail.com>
2025-04-30 16:45:55 -0700

    Function Expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,22 +1,36 @@
-//FUNCTIONS
-"use strict";
+// // FUNCTION EXPRESSIONS
 
-function logger() {
-  console.log("My name is Joan");
-}
-// Calling / Running / Invoking function
-// logger();
-// logger();
-// logger();
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
 
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges!`;
-  return juice;
-}
-console.log(fruitProcessor(3, 5));
+// const age1 = calcAge1(1991);
+// console.log(age1);
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+// console.log(calcAge2(2000));
+
+// //FUNCTIONS
+// "use strict";
+
+// function logger() {
+//   console.log("My name is Joan");
+// }
+// // Calling / Running / Invoking function
+// // logger();
+// // logger();
+// // logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges!`;
+//   return juice;
+// }
+// console.log(fruitProcessor(3, 5));
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
 // // Strict Mode
 // "use strict";
 
