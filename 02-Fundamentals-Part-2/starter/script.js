7edee2fb7697e8c18a6750e0b8d29dda964ac1d9
JoancitoGit <joan.garced@gmail.com>
2025-04-30 17:25:23 -0700

    Arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,18 +1,48 @@
+//ARRAYS
+
+const friends = ["Joan", "Samir", "David"];
+console.log(friends);
+
+// const years = new Array(1991, 1966, 1961, 1996);
+console.log(friends[0]);
+console.log(friends.length);
+console.log(friends[friends - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+const firstName = "Jonas";
+const jonas = [firstName, "Schmedtmann", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[2]);
+console.log(age1, age2, age3);
+
+const ages = [age1, age2, age3];
+console.log(ages);
+
 //CALLING FUNCTIONS INSIDE OTHER FUNCTIONS
 
-function cutFruitPieces(fruit) {
-  return fruit * 4;
-}
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
 
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
 
-  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
-  return juice;
-}
+//   const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+//   return juice;
+// }
 
-console.log(fruitProcessor(2, 3));
+// console.log(fruitProcessor(2, 3));
 
 // ARROW FUNCTION
 // const calcAge3 = (birthYear) => 2037 - birthYear;
